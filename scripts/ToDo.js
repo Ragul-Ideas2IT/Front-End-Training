6d1ade27e8a9bf598cdd0174c8d90237cdaa02c3
Ragul <ragul.venkatesan@ideas2it.com>
2022-12-28 20:49:46 +0530

    JavaScript implementation in task add

--- scripts/ToDo.js
+++ scripts/ToDo.js
@@ -1,33 +1,46 @@
+currentDate();
 function currentDate() {
-    var today = new Date();
-    var daylist = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday ",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    var monthlist = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    var date =
-      daylist[today.getDay()] +
-      ", " +
-      monthlist[today.getMonth()] +
-      " " +
-      today.getDate();
-    document.getElementById("today-date").innerHTML = date;
+  var today = new Date();
+  const daylist = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday ",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const monthlist = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  var date =
+    daylist[today.getDay()] +
+    ", " +
+    monthlist[today.getMonth()] +
+    " " +
+    today.getDate();
+  // console.log(date);
+  document.getElementById("today-date").innerHTML = date;
+}
+
+function addTask() {
+  document.querySelector("#tasks").innerHTML += `
+     <div class="task">
+     <span class="material-symbols-rounded"> list </span>
+         <span id="task-name">
+             ${document.querySelector("#newtask input").value}
+         </span>
+     </div>
+ `;
 }
