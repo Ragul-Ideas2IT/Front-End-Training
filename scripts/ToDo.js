21a82411e7501b5b3e6f274836a441f008be66f3
Ragul <ragul.venkatesan@ideas2it.com>
2023-01-05 00:24:52 +0530

    tasj and category rendering finished

--- scripts/ToDo.js
+++ scripts/ToDo.js
@@ -5,51 +5,67 @@
    */
   function init() {
     currentDate();
-    addTaskList();
-    // addTask();
     categoryRendering();
+    tasksRendering();
+    eventListener();
   }
 
+  var selectedCategory = "My Day";
+  var taskListInput = document.getElementById("tasklist-input");
+  var taskInput = document.getElementById("task-input");
+  var taskList = document.getElementById("side-menu");
+  var showHide = document.getElementById("show-hide");
+  var inputFromUser = document.getElementById("task-input");
+  var add = document.getElementById("add");
+  var tasks = [];
   var category = [
     {
+      id: 1,
       name: "My Day",
       icon: "wb_sunny",
     },
     {
+      id: 2,
       name: "Important",
       icon: "star",
     },
     {
+      id: 3,
       name: "Planned",
       icon: "calendar_month",
     },
     {
+      id: 4,
       name: "Assigned to me",
       icon: "person",
     },
     {
+      id: 5,
       name: "Tasks",
       icon: "home",
     },
   ];
 
-  var taskLists = [];
-  var selectedCategory;
+  function eventListener() {
+    taskInput.addEventListener("keypress", addTaskEvent);
+    taskListInput.addEventListener("keypress", addTaskListEvent);
+    showHide.addEventListener("click", hideSideMenu);
+    add.addEventListener("click", addTaskEvent);
+  }
 
   /**
    * It creates a list item for each category and appends it to the side menu
    */
   function categoryRendering() {
-    var taskList = document.getElementById("side-menu");
-    // taskList = "";
     for (let index = 0; index < category.length; index++) {
       const element = category[index];
       var list = document.createElement("li");
       var span = document.createElement("span");
       var div = document.createElement("div");
       var p = document.createElement("p");
+      list.id = element.id;
       list.className = "side-menu-option-container";
-      list.setAttribute("onclick", "task(this)");
+      list.addEventListener("click", selectedTask);
       span.className = "material-symbols-rounded";
       span.appendChild(document.createTextNode(element.icon));
       div.className = "menu-detail";
@@ -61,6 +77,94 @@
     }
   }
 
+  function tasksRendering() {
+    for (let index = 0; index < tasks.length; index++) {
+      const element = tasks[index];
+      var sublineDiv = document.createElement("div");
+      sublineDiv.className = "sub-line-task";
+      if (element.categories.includes(selectedCategory)) {
+        sublineDiv.appendChild(document.createTextNode(element.categories));
+        renderTask(element, sublineDiv);
+      }
+      // for (let index1 = 0; index1 < element.categories.length; index1++) {
+      //   const element1 = element.categories[index1];
+      //   if (selectedCategory == element1) {
+      //       if(element1 == "Important") {
+      //         sublineDiv.appendChild(document.createTextNode("Important"));
+      //       }
+      //       if(element1 == "My Day") {
+      //         sublineDiv.appendChild(document.createTextNode("My Day"));
+      //       }
+      //       if(element1 == "Tasks") {
+      //         sublineDiv.appendChild(document.createTextNode("Tasks"));
+      //       }
+      //     // sublineDiv.appendChild(document.createTextNode(element1));
+      //     renderTask(element, sublineDiv);
+      //   }
+      // }
+      // if(selectedCategory == element.category) {
+      //   if(element.isUnderImportant) {
+      //     sublineDiv.appendChild(document.createTextNode("Important"));
+      //   }
+      //   if(element.isUnderMyDay) {
+      //     sublineDiv.appendChild(document.createTextNode("My Day"));
+      //   }
+      //   if(element.isUnderTasks) {
+      //     sublineDiv.appendChild(document.createTextNode("Tasks"));
+      //   }
+      //   renderTask(element, sublineDiv);
+      // }
+      // if ((selectedCategory == "Important" && element.category == "Important") || element.isUnderImportant) {
+      //   sublineDiv.appendChild(document.createTextNode("Imp"));
+      //   renderTask(element, sublineDiv);
+      // }
+      // if ((selectedCategory == "My Day" && element.category == "My Day") || element.isUnderMyDay) {
+      //   sublineDiv.appendChild(document.createTextNode("My day"));
+      //   renderTask(element, sublineDiv);
+      // }
+      // if ((selectedCategory == "Tasks" && element.category == "Tasks") || element.isUnderTasks) {
+      //   sublineDiv.appendChild(document.createTextNode("Tasks"));
+      //   renderTask(element, sublineDiv);
+      // }
+    }
+  }
+
+  function renderTask(element, sublineDiv) {
+    var div = document.createElement("div");
+    var taskParentDiv = document.createElement("div");
+    var taskDiv = document.createElement("div");
+    var checkDiv = document.createElement("div");
+    var starDiv = document.createElement("div");
+    var starSpan = document.createElement("span");
+    var checkSpan = document.createElement("span");
+    var radio_button_unchecked = document.createTextNode(
+      "radio_button_unchecked"
+    );
+    var inputFromUserNode = document.createTextNode(element.name);
+    var star = document.createTextNode("star");
+    taskParentDiv.className = "task";
+    taskDiv.className = "task-added";
+    checkDiv.className = "check-container";
+    starDiv.className = "star-container";
+    checkSpan.className = "material-symbols-rounded";
+    starSpan.className = "material-symbols-rounded";
+    starSpan.id = "";
+    checkSpan.appendChild(radio_button_unchecked);
+    starSpan.appendChild(star);
+    checkSpan.addEventListener("click", makeTaskCompleted);
+    starSpan.addEventListener("click", makeImportant);
+    taskDiv.appendChild(inputFromUserNode);
+    var newTask = document.getElementById("tasks");
+    checkDiv.appendChild(checkSpan);
+    starDiv.appendChild(starSpan);
+    div.appendChild(checkDiv);
+    taskParentDiv.appendChild(taskDiv);
+    taskParentDiv.appendChild(sublineDiv);
+    div.appendChild(taskParentDiv);
+    div.appendChild(starDiv);
+    newTask.appendChild(div);
+    newTask.insertBefore(div, newTask.children[0]);
+  }
   /**
    * The function currentDate() is called when the page loads. It creates a new Date object, and then
    * uses the getDay(), getMonth(), and getDate() methods to get the current day, month, and date. It
@@ -107,151 +211,122 @@
    * with a div and a p element. The p element contains the text from the input field. The list item is
    * appended to the task list menu
    */
-  function addTaskList() {
-    var input = document.getElementById("tasklist-input");
-    //document.getElementById("side-menu") = "";
-    input.addEventListener("keypress", function (event) {
-      if (event.key === "Enter") {
-        // var list = document.createElement("li");
-        // var div = document.createElement("div");
-        // var p = document.createElement("p");
-        // list.innerHTML = '<span class="material-symbols-rounded"> list </span>';
-        var inputFromUser = 
-          document.getElementById("tasklist-input").value;
-        // if (inputFromUser == "") {
-        //   p.appendChild(document.createTextNode("Untitled"));
-        // } else {
-        //   p.appendChild(inputFromUser);
-        // }
-        // div.appendChild(p);
-        // list.setAttribute("onclick", "task(this)");
-        // list.appendChild(div);
-        // var newTask = document.getElementById("task-list-menu");
-        // newTask.appendChild(list);
-        category.push({name:inputFromUser, icon:"list"});
-        categoryRendering();
-        input.value = "";
+  function addTaskListEvent(event) {
+    if (event.key === "Enter") {
+      var inputFromUser = document.getElementById("tasklist-input").value;
+      var categoryLength = category.length;
+      console.log(categoryLength);
+      if (inputFromUser.trim() == "") {
+        category.push({
+          id: ++categoryLength,
+          name: "Untitled list",
+          icon: "list",
+        });
+      } else {
+        category.push({
+          id: ++categoryLength,
+          name: inputFromUser,
+          icon: "list",
+        });
       }
-    });
+      document.getElementById("side-menu").innerHTML = "";
+      categoryRendering();
+      taskListInput.value = "";
+    }
   }
 
+  /**
+   * When the user clicks the menu button, hide the side menu and change the menu button to a menu icon.
+   */
+  function hideSideMenu() {
+    var show = document.getElementById("show-menu");
+    var menu = document.getElementById("menu").children[0];
+    menu.innerHTML = "menu";
+    menu.addEventListener("click", showSideMenu);
+    show.id = "hide-menu";
+  }
+
+  /**
+   * When the user clicks the menu button, show the side menu.
+   */
+  function showSideMenu() {
+    var hide = document.getElementById("hide-menu");
+    hide.id = "show-menu";
+  }
+
+  function selectedTask(event) {
+    var id = event.target.id;
+    var title = category[id - 1].name;
+    var icon = category[id - 1].icon;
+    document.getElementById("content-icon").innerHTML = icon;
+    document.getElementById("content-category").innerHTML = title;
+    selectedCategory = title;
+    document.title = title;
+    document.getElementById("tasks").innerHTML = "";
+    tasksRendering();
+  }
+
+  function addTaskEvent(event) {
+    console.log(event.key);
+    if (event.key === "Enter") {
+      if (inputFromUser.value.trim() != "") {
+        var tasksElement = {
+          name: inputFromUser.value,
+          categories: [],
+        };
+        switch (selectedCategory) {
+          case "Important":
+            tasksElement.categories.push("Important");
+            tasksElement.categories.push("Tasks");
+            break;
+          case "My Day":
+            tasksElement.categories.push("My Day");
+            tasksElement.categories.push("Tasks");
+            break;
+          case "Tasks":
+            tasksElement.categories.push("Tasks");
+            break;
+          default:
+            tasksElement.categories.push(selectedCategory);
+        }
+        tasks.push(tasksElement);
+        document.getElementById("tasks").innerHTML = "";
+        tasksRendering();
+        taskInput.value = "";
+      }
+    }
+  }
+
+  function makeImportant(event) {
+    var id = event.target;
+    id.id = "filled";
+    id.removeEventListener("click", makeImportant);
+    id.addEventListener("click", makeUnimportant);
+  }
+  
+  function makeUnimportant(event) {
+    var id = event.target;
+    id.id = "";
+    id.removeEventListener("click", makeUnimportant);
+    id.addEventListener("click", makeImportant);
+  }
+  
+  function makeTaskCompleted(event) {
+    var id = event.target;
+    id.innerHTML = "check_circle";
+    id.id = "filled";
+    id.removeEventListener("click", makeTaskCompleted);
+    id.addEventListener("click", makeTaskNotCompleted);
+  }
+  
+  function makeTaskNotCompleted(event) {
+    var id = event.target;
+    id.innerHTML = "radio_button_unchecked";
+    id.id = "";
+    id.removeEventListener("click", makeTaskNotCompleted);
+    id.addEventListener("click", makeTaskCompleted);
+  }
   init();
 })();
 
-/**
- * When the user clicks on a task, the title of the page changes to the title of the task.
- * @param id - The id of the element that was clicked.
- */
-function task(id) {
-  document.getElementById("menu").innerHTML = id.innerHTML;
-  var title = id.children[1].children[0].innerHTML;
-  selectedCategory = title;
-  document.title = title;
-}
 
-/**
- * When the user clicks the menu button, hide the side menu and change the menu button to a menu icon.
- */
-function hideSideMenu() {
-  // document.getElementsByClassName("side-container")[0].style.display = "none";
-  // document.querySelector("#menu .material-symbols-rounded").innerHTML = "menu";
-  // document
-  //   .querySelector("#menu .material-symbols-rounded")
-  //   .setAttribute("onclick", "showSideMenu()");
-  var show = document.getElementById("show-menu");
-  var menu = show.children[0].children[0];
-  menu.setAttribute("onclick", "showSideMenu()");
-  show.id = "hide-menu";
-}
-
-/**
- * When the user clicks the menu button, show the side menu.
- */
-function showSideMenu() {
-  // document.getElementsByClassName("side-container")[0].style.display =
-  //   "inline-block";
-  // document.querySelector("#menu .material-symbols-rounded").innerHTML = "menu";
-  var hide = document.getElementById("hide-menu");
-  var menu = hide.children[0].children[0];
-  menu.setAttribute("onclick", "hideSideMenu()");
-  hide.id = "show-menu";
-}
-
-function makeImportant(id) {
-  id.id = "filled";
-  id.setAttribute("onclick", "makeUnimportant(this)");
-}
-
-function makeTaskCompleted(id) {
-  id.innerHTML = "check_circle";
-  id.id = "filled";
-  id.setAttribute("onclick", "makeTaskNotCompleted(this)");
-}
-
-function makeUnimportant(id) {
-  id.id = "";
-  id.setAttribute("onclick", "makeImportant(this)");
-}
-
-function makeTaskNotCompleted(id) {
-  id.innerHTML = "radio_button_unchecked";
-  id.id = "";
-  id.setAttribute("onclick", "makeTaskCompleted(this)");
-}
-
-/**
- * The function addTask() adds an event listener to the input element with the id "task-input". The
- * event listener listens for the keypress event. When the keypress event is triggered, the function
- * addTaskInHtml() is called
- */
-function addTask() {
-  console.log(
-    document.getElementById("menu").children[1].children[0].innerHTML
-  );
-  var input = document.getElementById("task-input");
-  var taskCategory =
-    document.getElementById("menu").children[1].children[0].textContent;
-  input.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-      var div = document.createElement("div");
-      var taskParentDiv = document.createElement("div");
-      var taskDiv = document.createElement("div");
-      var sublineDiv = document.createElement("div");
-      var checkDiv = document.createElement("div");
-      var starDiv = document.createElement("div");
-      var starSpan = document.createElement("span");
-      var checkSpan = document.createElement("span");
-      var radio_button_unchecked = document.createTextNode(
-        "radio_button_unchecked"
-      );
-      var star = document.createTextNode("star");
-      taskParentDiv.className = "task";
-      taskDiv.className = "task-added";
-      sublineDiv.className = "sub-line-task";
-      checkDiv.className = "check-container";
-      starDiv.className = "star-container";
-      checkSpan.className = "material-symbols-rounded";
-      starSpan.className = "material-symbols-rounded";
-      checkSpan.appendChild(radio_button_unchecked);
-      starSpan.appendChild(star);
-      checkSpan.setAttribute("onclick", "makeTaskCompleted(this)");
-      starSpan.setAttribute("onclick", "makeImportant(this)");
-      var inputFromUser = document.createTextNode(
-        document.getElementById("task-input").value
-      );
-      taskDiv.appendChild(inputFromUser);
-      var newTask = document.getElementById("tasks");
-      sublineDiv.appendChild(document.createTextNode(selectedCategory));
-      checkDiv.appendChild(checkSpan);
-      starDiv.appendChild(starSpan);
-      div.appendChild(checkDiv);
-      taskParentDiv.appendChild(taskDiv);
-      taskParentDiv.appendChild(sublineDiv);
-      div.appendChild(taskParentDiv);
-      div.appendChild(starDiv);
-      newTask.appendChild(div);
-      newTask.insertBefore(div, newTask.children[0]);
-      input.value = "";
-    }
-  });
-}
